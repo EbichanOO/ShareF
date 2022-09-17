d7bfa7a851acf4dc35b13b40a408610b69184238
unknown <takanori.sao@icloud.com>
2022-09-18 00:02:02 +0900

    [add] http get script

--- Assets/Samples/Pretia SDK/0.6.0/Map-based Relocalization Sample/Scripts/GenerateCommentObject.cs
+++ Assets/Samples/Pretia SDK/0.6.0/Map-based Relocalization Sample/Scripts/GenerateCommentObject.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
 public class GenerateCommentObject : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        var client = new HttpClient();
+        HttpResponseMessage result = client.GetAsync(@"http://35.239.223.188:5000/init/object").Result;
+        var resultBody = result.Content.ReadAsStringAsync().Result;
+        Debug.Log(resultBody);
+
         for(int i=0; i< 2; i++) {
             // CubeプレハブをGameObject型で取得
             GameObject obj = (GameObject)Resources.Load("Cube");
